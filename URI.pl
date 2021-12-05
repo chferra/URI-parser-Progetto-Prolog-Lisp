750b0e0c77f8376a1a13e0118bda1e5e5ac38851
FerraChris01 <ferrareis.christian@gmail.com>
2021-12-05 16:39:13 +0100

    Update URI.pl

--- URI.pl
+++ URI.pl
@@ -1,4 +1,4 @@
-inputTxt(T, X) :- string_chars(T, L), port(L, X).
+inputTxt(T, X) :- string_chars(T, L), path(L, X).
 
 
 fragment(X, Y) :- caratteri(X, Y, []).
@@ -16,8 +16,13 @@ port([D|Ds], Y) :- digit(D), !, port(Ds, R), atom_concat(D, R, Y).
 
 digit(C) :- atom_number(C, D), D >= 0, D =< 9, !.
 
-path([X], X) :- identificatore(X.
-path([X| Xs], Y) :- identificatore(X
+path([X], Y) :- identificatore(X, Y), !.
+path(['/'|Xs], Z) :- path(Xs, Y), !, atom_concat('/', Y, Z).
+path([X|Xs], Z) :- identificatore(X, Y), !, path(Xs, R), atom_concat(Y, R, Z).
+
+terna(X, Y, Z, R) :- digit(X), digit(Y), digit(Z), R is X * 100 + Y * 10 + Z, R >= 0, R =< 255.
+
+indirizzo-ip([X|Xs],Y) :- X 
 
 reserved(C) :- gen-delims(C); sub-delims(C).
 gen-delims(C) :- member(C, [':','/','?','#','[',']','@']).
