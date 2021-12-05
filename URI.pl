c5d6fa95c7fe27958e9b847cb916ce918d1a92fa
FerraChris01 <ferrareis.christian@gmail.com>
2021-12-05 15:38:11 +0100

    update

--- URI.pl
+++ URI.pl
@@ -1,27 +1,33 @@
-inputTxt(T) :- string_chars(T, L), port(L, X).
+inputTxt(T, X) :- string_chars(T, L), port(L, X).
 
-digit(D) :- number(D), D >= 0, D =< 9.
 
-identificatore([I|Is]) :- !, identificatore(I), !, identificatore(Is).
-identificatore(I) :- non_member(I, ['/','?','#','@',':']).
+fragment(X, Y) :- caratteri(X, Y, []).
+identificatore(X, Y) :- caratteri(X, Y, ['/','?','#','@',':']).
+identificatore-host(X, Y) :- caratteri(X, Y, ['.','/','?','#','@',':']).
+query(X, Y) :- caratteri(X, Y, ['#']).
 
-identificatore-host([I|Is]) :- !, identificatore-host(I), !, identificatore-host(Is).
-identificatore-host(I) :- non_member(I, ['.','/','?','#','@',':']).
+caratteri([],'',_).
+caratteri([C|Cs], R, Filtri) :- non_member(C,Filtri), carattere(C), !, caratteri(Cs, S, Filtri), atom_concat(C, S, R).
+carattere(C) :- reserved(C); unreserved(C).
 
-query([]).
-query([Q|Qs]) :- query(Q), query(Qs).
-query(Q) :- Q \= '#'.
+port([],80).
+port([D],D) :- digit(D), !.
+port([D|Ds], Y) :- digit(D), !, port(Ds, R), atom_concat(D, R, Y).
 
-port([], 80).
-port([X], X) :- digit(X), !.
-port([X|Xs], R) :- digit(X), port(Xs, N), atom_concat(X, N, R).
+digit(C) :- atom_number(C, D), D >= 0, D =< 9, !.
+
+path([X], X) :- identificatore(X.
+path([X| Xs], Y) :- identificatore(X
+
+reserved(C) :- gen-delims(C); sub-delims(C).
+gen-delims(C) :- member(C, [':','/','?','#','[',']','@']).
+sub-delims(C) :- member(C, ['!','$','&','\'','(',')','*','+',',',';','=']).
+
+unreserved(C) :- digit(C); lower_az(C); upper_az(C); member(C, ['-','.','_','~']).
+
+lower_az(C) :- char_code(C, N), N >= 97, N =< 122, !.
+upper_az(C) :- char_code(C, N), N >= 65, N =< 90, !.
 
 non_member(X, [X|_]) :- !, fail.
 non_member(X, [_|Xs]) :- !, non_member(X, Xs).
 non_member(_, []).
-
-
-
-
-
-
