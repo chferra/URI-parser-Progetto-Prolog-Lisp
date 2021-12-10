d0b358dbac61c25bf6a6ff675e4861b6a5e3ddb3
ChrisFerra01 <ferrareis.christian@gmail.com>
2021-12-10 11:09:47 +0100

    tolta roba inutile

--- URI.pl
+++ URI.pl
@@ -1,35 +1,60 @@
-inputTxt(T, X) :- string_chars(T, L), path(L, X).
+uri(S, UI, H, Port, Path, Q, F).
 
-uri(Scheme, Userinfo, Host, Port, Path, Query, Fragment).
+uri_parse(L, uri(S, UI, H, Port, Path, Q, F)) :- 
+						string_chars(L, URI), 
+						scheme(URI, URIexcS, S),
+						authority(URIexcS, URIexcSA, UI, H, Port),
+						pqf(URIexcSA, [], Path, Q, F).
+
+scheme(X, Rest, Result) :- identificatore(X, [':'|Rest], Result).
+
+authority(['/','/'|Xs], Rest, UI, H, P) :-
+								userinfo(Xs, RestUI, UI), 
+								host(RestUI, RestH, H),
+								port(RestH, Rest, P), !.
+								
+userinfo(X, Rest, Result) :- identificatore(X, ['@'|Rest], Result), !.
+userinfo(Rest, Rest, '').
+								
+pqf([],[],'','',''). 
+pqf(['/'|Xs], Rest, P, Q, F) :- 
+								path(Xs, RestP, P),
+								query(RestP, RestQ, Q),
+								fragment(RestQ, Rest, F), !.
+
+path(Xs, Rest, Result) :- identificatore(Xs, Rest, Result).
+path(Rest, Rest, '').
+
+query(['?'|Xs], Rest, Result) :- caratteri(Xs, Rest, Result, ['#']),
+								Result \= '', !.
+query(Rest, Rest, '').
+
+fragment(['#'|Xs], Rest, Result) :- caratteri(Xs, Rest, Result, []),
+								Result \= '', !.
+fragment(Rest, Rest, '').
 
 
+host(H, Rest, Result) :- identificatore-host(H, Rest, Result). 
 
+identificatore(X, Rest, Result) :- caratteri(X, Rest, Result, ['/','?','#','@',':']), 
+									Result \= ''.
+identificatore-host(X, Rest, Result) :- caratteri(X, Rest, Result, ['.','/','?','#','@',':']),
+										Result \= ''.
 
+inputTxt(T, Rest, Result) :- string_chars(T, L), port(L, Rest, Result).
 
-fragment(X, Y) :- caratteri(X, Y, []).
-identificatore(X, Y) :- caratteri(X, Y, ['/','?','#','@',':']).
-identificatore-host(X, Y) :- caratteri(X, Y, ['.','/','?','#','@',':']).
-query(X, Y) :- caratteri(X, Y, ['#']).
+port([':'|Xs], Rest, Result) :- digits(Xs, Rest, Result),
+								Result \= '', !.
+port(Rest, Rest, 80).
 
-caratteri([],'',_).
-caratteri([C|Cs], R, Filtri) :- non_member(C,Filtri), carattere(C), !, caratteri(Cs, S, Filtri), atom_concat(C, S, R).
+caratteri([C|Cs], Rest, Result, Filtri) :- 
+								non_member(C,Filtri), !,
+								carattere(C), !, 
+								caratteri(Cs, Rest, R, Filtri), !,
+								atom_concat(C, R, Result).
+caratteri(Rest, Rest, '', _).
+
 carattere(C) :- reserved(C); unreserved(C).
-
-port([],80).
-port([D],D) :- digit(D), !.
-port([D|Ds], Y) :- digit(D), !, port(Ds, R), atom_concat(D, R, Y).
-
-digit(C) :- atom_number(C, D), D >= 0, D =< 9, !.
-
-
-path([X], Y) :- identificatore(X, Y), !.
-path(['/'|Xs], Z) :- path(Xs, Y), !, atom_concat('/', Y, Z).
-path([X|Xs], Z) :- identificatore(X, Y), !, path(Xs, R), atom_concat(Y, R, Z).
-
-%terna(X, Y) :- gruppo(X, Y, 3). 
-%gruppo([],'',0).
-%gruppo([X|Xs], S, N) :- write(X), N > 0, digit(X), gruppo(Xs, T, P), N is P + 1, atom_concat(X,T,S).
-
 
 reserved(C) :- gen-delims(C); sub-delims(C).
 gen-delims(C) :- member(C, [':','/','?','#','[',']','@']).
@@ -40,6 +65,70 @@ unreserved(C) :- digit(C); lower_az(C); upper_az(C); member(C, ['-','.','_','~']
 lower_az(C) :- char_code(C, N), N >= 97, N =< 122, !.
 upper_az(C) :- char_code(C, N), N >= 65, N =< 90, !.
 
+digits([D|Ds], Rest, Result) :- digit(D), !,
+								digits(Ds,Rest,R), 
+								atom_concat(D,R,Result). 
+digits(Rest,Rest,'').
+digit(C) :- atom_number(C, D), D >= 0, D =< 9, !.
+
 non_member(X, [X|_]) :- !, fail.
 non_member(X, [_|Xs]) :- !, non_member(X, Xs).
 non_member(_, []).
+
+%%% 
+%%% Display URI components
+%%%
+uri_display(L) :- 
+						string_chars(L, URI), 
+						scheme(URI, URIexcS, S),
+						authority(URIexcS, URIexcSA, UI, H, Port),
+						pqf(URIexcSA, [], Path, Q, F),
+						write('Schema: '),
+						write(S),
+						nl,
+						write('UserInfo: '),
+						write(UI),
+						nl,
+						write('Host: '),
+						write(H),
+						nl,
+						write('Port: '),
+						write(Port),
+						nl,
+						write('Path: '),
+						write(Path),
+						nl,
+						write('Query: '),
+						write(Q),
+						nl,
+						write('Fragment: '),
+						write(F),
+						nl.
+
+uri_display(L, uri(S, UI, H, Port, Path, Q, F)) :- 
+						string_chars(L, URI), 
+						scheme(URI, URIexcS, S),
+						authority(URIexcS, URIexcSA, UI, H, Port),
+						pqf(URIexcSA, [], Path, Q, F),
+						write('Schema: '),
+						write(S),
+						nl,
+						write('UserInfo: '),
+						write(UI),
+						nl,
+						write('Host: '),
+						write(H),
+						nl,
+						write('Port: '),
+						write(Port),
+						nl,
+						write('Path: '),
+						write(Path),
+						nl,
+						write('Query: '),
+						write(Q),
+						nl,
+						write('Fragment: '),
+						write(F),
+						nl.
+						
