a1fe32dc02189448e30c319f0f29e7ba8755305b
FerraChris01 <ferrareis.christian@gmail.com>
2021-12-29 01:04:42 +0100

    aggiunto identificatore

--- Lisp/uri-parse.lisp
+++ Lisp/uri-parse.lisp
@@ -26,25 +26,51 @@
                    :fragment (car fragment))))
 
 (defun parse-scheme (l)
-  (list (car l) (cdr l)))
+  (let ((res (identificatore l)))
+    (list (car res) (second res))))
+
+(defparameter gen-delims
+  (list #\: #\/ #\? #\# #\[ #\] #\@))
+(defparameter sub-delims
+  (list #\! #\$ #\& #\' #\( #\) #\* #\+ #\, #\; #\=))
+(defparameter unres-symb
+  (list #\- #\. #\_ #\~))
 
 (defun parse-userinfo (l)
-  (list (car l) (cdr l)))
+  (list (car l) (second l)))
 
 (defun parse-host (l)
-  (list (car l) (cdr l)))
+  (list (car l) (second l)))
 
 (defun parse-port (l)
-  (list (parse-integer (car l)) (cdr l)))
+  (list (parse-integer (car l)) (second l)))
 
 (defun parse-path (l)
-  (list (car l) (cdr l)))
+  (list (car l) (second l)))
 
 (defun parse-query (l)
-  (list (car l) (cdr l)))
+  (list (car l) (second l)))
 
 (defun parse-fragment (l)
-  (list (car l) (cdr l)))
+  (list (car l) (second l)))
 
 (defun deleteFirst (lista)
-  (append nil (cdr lista)))
+  (append nil (second lista)))
+
+(defun identificatore (x)
+  (let ((res (caratteri x '(#\/ #\? #\# #\@ #\:))))
+    (list (car res) (second res))))
+
+(defun caratteri (chrs filtri)
+  (let ((i (car chrs))
+        (rest (cdr chrs)))
+    (cond ((or (null i)
+               (member i filtri))
+           (list nil chrs))
+          ((or
+            (alphanumericp i) ;unreserved
+            (member i unres-symb)
+            (member i gen-delims) ;reserved
+            (member i sub-delims)) 
+           (let ((res (caratteri (cdr chrs) filtri)))
+             (list (append (list i) (car res)) (second res)))))))
