3618fa199f87b4108e3367c1fc16efb6b10edca5
Davide0021 <davide3006@libero.it>
2022-01-02 20:04:28 +0100

    Aggiunto secondo file con correzioni
    Penso di avere corretto il problema di evitare "mailtO" rispetto a "mailto". Inoltre ho eliminato gli "if" sostituendoli opportunamente con i "cond"

--- Lisp/inputTextData.lisp
+++ Lisp/inputTextData.lisp
@@ -1,14 +1,14 @@
 %
 %  Chiamate corrette
 %
-(uri-parse "http://Uinfo@HostX:85/myServer/line/one?QueryXyZ#Fragment-01")	;XXXXX       Error: In a call to LENGTH of (#\m #\y #\S #\e #\r #\v #\e #\r #\/ . "line/one"), tail "line/one" is not a LIST
-(uri-parse "http://Uinfo@HostX:85/myServer/line/one?QueryXyZ")	;XXXXX    
-(uri-parse "http://Uinfo@HostX:85/myServer/line/one#Fragment-01")	;XXXXX   
-(uri-parse "http://Uinfo@HostX/myServer/line/one")	;XXXXX	
-(uri-parse "http://HostX/myServer/line/one")	;XXXXX	
-(uri-parse "http://HostX.bis/myServer/line/one")	;XXXXX	
-(uri-parse "http://192.168.1.34/myServer/line/one")	;XXXXX	
-(uri-parse "http://Uinfo@192.168.1.34/myServer/line/one")	;XXXXX 
+(uri-parse "http://Uinfo@HostX:85/myServer/line/one?QueryXyZ#Fragment-01")	;V
+(uri-parse "http://Uinfo@HostX:85/myServer/line/one?QueryXyZ")	;V
+(uri-parse "http://Uinfo@HostX:85/myServer/line/one#Fragment-01")	;V 
+(uri-parse "http://Uinfo@HostX/myServer/line/one")	;V	
+(uri-parse "http://HostX/myServer/line/one")	;V	
+(uri-parse "http://HostX.bis/myServer/line/one")	;V	
+(uri-parse "http://192.168.1.34/myServer/line/one")	;V	
+(uri-parse "http://Uinfo@192.168.1.34/myServer/line/one")	;V 
 
 (uri-parse "http://Uinfo@HostX:85/myServer?QueryXyZ#Fragment-01)	;V
 (uri-parse "http://Uinfo@HostX/myServer")	;V
@@ -18,8 +18,8 @@
 (uri-parse "http://Uinfo@HostX")	;V
 (uri-parse "http://HostX/myServer") 	;V
 
-(uri-parse "http://Uinfo/myServer/line/one?QueryXyZ#Fragment-01")	;XXXXX
-(uri-parse "http://Uinfo@Host/myServer/line/one?Query#XyZ#Fragment-01")	;XXXXX
+(uri-parse "http://Uinfo/myServer/line/one?QueryXyZ#Fragment-01")	;V
+(uri-parse "http://Uinfo@Host/myServer/line/one?Query#XyZ#Fragment-01")	;V
 
 (uri-parse "mailto:Uinfo@HostX")	;V
 (uri-parse "mailto:Uinfo@HostX.yz")	;V
@@ -54,11 +54,12 @@
 (uri-parse "http://Uinfo@Host:/myServer/line/one?QueryXyZ#Fragment-01")	;V
 (uri-parse "http://Uinfo@HostX.:85/myServer/line/one?QueryXyZ#Fragment-01")	;V
 (uri-parse "http://Uinfo@HostX.A:8s/myServer/line/one?QueryXyZ#Fragment-01")	;V
-(uri-parse "http://Uinfo@HostX/myServer/line/o@ne?QueryXyZ#Fragment-01")	;XXXXX
+(uri-parse "http://Uinfo@HostX/myServer/line/o@ne?QueryXyZ#Fragment-01")	;V
 
 (uri-parse "news:HostsX/A")	;V
 (uri-parse "news:HostsX:A")	;V
 (uri-parse "mailtO:Uinfo@HostX")	;XXXXX   Non discrimina la "o" maiuscola  >> forse in Lisp OK
+                                    ;Y corretto in uri-parseNew alla riga 249
 (uri-parse "mailto:Uinfo@HostX.")	;V
 (uri-parse "mailto://Uinfo@HostX/myServer/line/one?QueryXyZ#Fragment-01")	;V
 (uri-parse "mailto:Uinfo@HostX/myServer")	;V
@@ -68,11 +69,11 @@
 
 
 (uri-parse "zos://Uinfo@")	;V
-(uri-parse "zos://Uinfo@HostX.yz/")	;XXXXX      (uri-parse "zos://Uinfo@HostX.yz/")	
+(uri-parse "zos://Uinfo@HostX.yz/")	;XXXXX     Error: NIL (of type NULL) is not of type CHARACTER.
 (uri-parse "zos://Uinfo@HostX:85/.?QueryXyZ#Fragment-01")	;V
 (uri-parse "zos://Uinfo@HostX:85/.(id8Val)#Fragment-01")	;V
 (uri-parse "zos://Uinfo@HostX:85/IDV(id8).ID44val(id8Val)?QueryXyZ#Fragment-01")    ;V
 (uri-parse "zos://Uinfo@HostX:85/IDV.ID44val.?QueryXyZ#Fragment-01")    ;V
-(uri-parse "zos://Uinfo@200.200.201.203/ID441234567890123456789012345678901234567890A?QueryXyZ#Fragment-01")    ;XXXXX    Stack overflow (stack size 15997). 
+(uri-parse "zos://Uinfo@200.200.201.203/ID441234567890123456789012345678901234567890A?QueryXyZ#Fragment-01")    ;XXXXX    NON vede il superamento dei 44 cratteri per "id44"
 (uri-parse "zos://Uinfo@HostX/1IDV.ID44val?QueryXyZ#Fragment-01")    ;V
 
