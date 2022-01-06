a063e88a08d8d7d82a926278ad62f172eb5b7720
FerraChris01 <ferrareis.christian@gmail.com>
2022-01-06 22:28:09 +0100

    fix error

--- Lisp/uri-parse.lisp
+++ Lisp/uri-parse.lisp
@@ -53,7 +53,7 @@
 (defun uri-scheme (uriS-p)
   (cond ((uri-structure-p uriS-p)
          (uri-structure-scheme uriS-p))
-        (T (error "Il parametro deve essere una uri-structure"))))
+        (T (error "Il parametro deve essere una uri-structure valida"))))
 
 (defun parse-mailto (scheme)
   (let* ((ui (parse-userinfo2 scheme))
